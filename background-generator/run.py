19d382f583024cb2687900cceaee2e8d1bd182fb
sndpkiran <sndpkiran@gmail.com>
2017-01-03 22:55:43 +0530

    generates an image with random background and takes resolution as command line arguments

--- background-generator/run.py
+++ background-generator/run.py
@@ -1,8 +1,22 @@
 from PIL import Image
+import random
+import sys
 
 def main():
-	img = Image.new("RGB", (512, 512), "red")
-	img.save("image_1", "JPEG")
+	if len(sys.argv) != 3:
+		print "\nUsage: \n$ python run.py <screen_width> <screen_height>\n"
+		exit()
+
+	# get the output image resolution as command line arguments
+	img_res = (int(sys.argv[1]), int(sys.argv[2]))
+	img_format = "JPEG"
+	colors = ("#EF5350", "#42A5F5", "#66BB6A", "#FFEE58", "#78909C")
+
+	# set a random color as background
+	img = Image.new("RGB", img_res, colors[random.randint(0, 4)])
+
+	# save the image as a jpeg file
+	img.save("image_1." + img_format, img_format)
 
 if __name__ == "__main__":
 	main()
